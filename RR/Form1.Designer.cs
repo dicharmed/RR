b273bb42bc526503883180ddef09631a0df890f8
Diana <dicharmed@mail.ru>
2019-04-01 22:57:40 +0400

    first commit

---
+++ RR/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace RR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(422, 460);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(463, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 283);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(194, 37);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(51, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(194, 238);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(51, 20);
+            this.textBox7.TabIndex = 13;
+            this.textBox7.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox7_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(87, 241);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Квант времени: ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(194, 196);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(51, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(171, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Время выполнения процесса 5: ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(194, 173);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(51, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Время выполнения процесса 4: ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(194, 150);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(51, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Время выполнения процесса 3: ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(194, 127);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(51, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Время выполнения процесса 2: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(194, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(51, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Время выполнения процесса 1: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество процессов (макс. 5): ";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(553, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(553, 407);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(463, 301);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(293, 58);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(768, 484);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "RR";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ RR/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void onlyDigits(KeyPressEventArgs e)
+        {            
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        void readOnly_txtBoxes(bool n, int c) // n(show/hide); c(amount of process)
+        {
+            textBox2.ReadOnly = n;
+
+            if (c == 2)
+            {
+                textBox2.ReadOnly = n;
+                textBox3.ReadOnly = n;
+            }
+            else if (c == 3)
+            {
+                textBox2.ReadOnly = n;
+                textBox3.ReadOnly = n;
+                textBox4.ReadOnly = n;
+            }
+            else if(c == 4)
+            {
+                textBox2.ReadOnly = n;
+                textBox3.ReadOnly = n;
+                textBox4.ReadOnly = n;
+                textBox5.ReadOnly = n;
+            }else
+            {
+                textBox2.ReadOnly = n;
+                textBox3.ReadOnly = n;
+                textBox4.ReadOnly = n;
+                textBox5.ReadOnly = n;
+                textBox6.ReadOnly = n;
+            }
+
+        }
+      
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e) 
+        {
+            readOnly_txtBoxes(true, 5); //hide txtBoxes if value's changed
+
+            int num = 1; //amount of proccess by default
+
+            if (numericUpDown1 != null)
+            {
+                num = Convert.ToInt32(numericUpDown1.Value);
+                readOnly_txtBoxes(false, num); //show txtBoxes
+            }
+           
+        }
+
+        int[] readTxtBoxes()
+        {
+            //read quantum & time of running of processes
+            int processes = Convert.ToInt32(numericUpDown1.Value);
+
+            int p1, p2, p3, p4, p5;
+
+            
+            int[] btime = new int[processes];
+
+
+            if (processes == 5)
+            {
+                p1 = Convert.ToInt32(textBox2.Text);
+                p2 = Convert.ToInt32(textBox3.Text);
+                p3 = Convert.ToInt32(textBox4.Text);
+                p4 = Convert.ToInt32(textBox5.Text);
+                p5 = Convert.ToInt32(textBox6.Text);
+
+                btime[0] = p1;
+                btime[1] = p2;
+                btime[2] = p3;
+                btime[3] = p4;
+                btime[4] = p5;
+            }
+            else if (processes == 4)
+            {
+                p1 = Convert.ToInt32(textBox2.Text);
+                p2 = Convert.ToInt32(textBox3.Text);
+                p3 = Convert.ToInt32(textBox4.Text);
+                p4 = Convert.ToInt32(textBox5.Text);
+
+                btime[0] = p1;
+                btime[1] = p2;
+                btime[2] = p3;
+                btime[3] = p4;
+            }
+            else if (processes == 3)
+            {
+                p1 = Convert.ToInt32(textBox2.Text);
+                p2 = Convert.ToInt32(textBox3.Text);
+                p3 = Convert.ToInt32(textBox4.Text);
+
+                btime[0] = p1;
+                btime[1] = p2;
+                btime[2] = p3;
+
+            }
+            else if (processes == 2)
+            {
+                p1 = Convert.ToInt32(textBox2.Text);
+                p2 = Convert.ToInt32(textBox3.Text);
+
+                btime[0] = p1;
+                btime[1] = p2;
+
+            }
+            else
+            {
+                p1 = Convert.ToInt32(textBox2.Text);
+
+                btime[0] = p1;
+
+            }
+
+            return btime;
+        }
+
+        private void button1_Click(object sender, EventArgs e) // start
+        {    
+            int quantum = 1;
+            quantum = Convert.ToInt32(textBox7.Text);
+
+            int processes = Convert.ToInt32(numericUpDown1.Value);
+           
+            int[] rtime = readTxtBoxes();              
+
+            int rp = processes;
+            int i = 0;
+            int time = 0;
+
+            do
+            {
+                if (rtime[i] > quantum)//if time of process's running is bigger than quantum
+                {
+                    rtime[i] = rtime[i] - quantum;                    
+                    time += quantum;
+                    //cout << time;                    
+                    textBox1.Text += "p" + (i+1) + ": " + time + " ";
+                }
+                else if (rtime[i] <= quantum && rtime[i] > 0)
+                {
+                    time += rtime[i];
+                    rtime[i] = rtime[i] - rtime[i];                    
+                    rp--;
+                    // cout << time;
+                    if(rp!=0)textBox1.Text += "p" + (i+1) + ": " + time + " ";
+                }
+
+                i++;
+                if (i == processes)
+                {
+                    i = 0;
+                }
+
+            } while (rp != 0); //till all processes is done
+        }
+
+        private void button2_Click(object sender, EventArgs e) //clear all data
+        {
+            textBox2.Text = null;
+            textBox3.Text = null;
+            textBox4.Text = null;
+            textBox5.Text = null;
+            textBox6.Text = null;
+            textBox7.Text = null;
+
+            //pictureBox
+        }
+
+        //----------------
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyDigits(e);
+        }
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyDigits(e);
+        }
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyDigits(e);
+        }
+        private void textBox5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyDigits(e);
+        }
+        private void textBox6_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            onlyDigits(e);
+        }
+        private void textBox7_KeyPress(object sender, KeyPressEventArgs e) //txtBox quantum
+        {           
+            onlyDigits(e);
+            
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+    }
+}
