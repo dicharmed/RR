f1a93e86cb41767e98c6564baf96dcada8e0f399
Diana <dicharmed@mail.ru>
2019-04-05 17:57:29 +0400

    first commit

---
+++ OS Project/Form2.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace rr_program
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.switch_time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.quantum_Box = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quantum_Box)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(67)))), ((int)(((byte)(53)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 383);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(803, 26);
+            this.panel2.TabIndex = 24;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(67)))), ((int)(((byte)(53)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(803, 26);
+            this.panel1.TabIndex = 23;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(67)))), ((int)(((byte)(53)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(241, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.RaisedVertical;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name,
+            this.time,
+            this.switch_time,
+            this.status});
+            this.dataGridView.Location = new System.Drawing.Point(0, 32);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersWidth = 4;
+            this.dataGridView.Size = new System.Drawing.Size(400, 218);
+            this.dataGridView.TabIndex = 25;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Название";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // time
+            // 
+            this.time.HeaderText = "Время выполнения";
+            this.time.Name = "time";
+            this.time.ReadOnly = true;
+            // 
+            // switch_time
+            // 
+            this.switch_time.HeaderText = "Время переключения";
+            this.switch_time.Name = "switch_time";
+            this.switch_time.ReadOnly = true;
+            // 
+            // status
+            // 
+            this.status.HeaderText = "Статус";
+            this.status.Name = "status";
+            this.status.ReadOnly = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(67)))), ((int)(((byte)(53)))));
+            this.label2.Location = new System.Drawing.Point(54, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 18);
+            this.label2.TabIndex = 78;
+            this.label2.Text = "Введите квант :";
+            // 
+            // quantum_Box
+            // 
+            this.quantum_Box.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quantum_Box.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(67)))), ((int)(((byte)(53)))));
+            this.quantum_Box.Location = new System.Drawing.Point(213, 273);
+            this.quantum_Box.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.quantum_Box.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.quantum_Box.Name = "quantum_Box";
+            this.quantum_Box.Size = new System.Drawing.Size(130, 24);
+            this.quantum_Box.TabIndex = 1;
+            this.quantum_Box.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(67)))), ((int)(((byte)(53)))));
+            this.label1.Location = new System.Drawing.Point(244, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 79;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(427, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(364, 345);
+            this.pictureBox1.TabIndex = 80;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(133)))), ((int)(((byte)(244)))));
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(57, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 32);
+            this.button2.TabIndex = 81;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 409);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.quantum_Box);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(500, 170);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quantum_Box)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown quantum_Box;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn switch_time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
+    }
+}
---
+++ OS Project/RoundRobin.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rr_program
+{
+    public class RoundRobin
+    {
+        DataGridView dataGridView;
+         
+        public PictureBox pictureBox1 { get; set; }
+
+        //----------------RoundRobin Class Constructor-------------------
+        public RoundRobin(ref DataGridView temp_dataGridView, PictureBox p)
+        {
+            dataGridView = temp_dataGridView;
+            pictureBox1 = p;
+        }
+
+        //----------------Main Round Robin Algorithm Method-------------------
+        public void runRoundRobin(ref NewProcess[] multiNewProcesses, int quantum)
+        {
+            int hv = 0, wh = 0, ww = 0; //for drawing rect; theyre like x, y, x.
+          
+            //Assign Each Process Its Execution Time
+            foreach (var NewProcess in multiNewProcesses)
+            {
+                //RemaingTime = Total Time @ when process starts execution
+                //because of the switch time (время переключения) its given in % so like if, for example, sw =10% from time, so remaining time =
+                NewProcess.remainingTime = NewProcess.time - (NewProcess.time * NewProcess.switchTime / 100);
+
+                drawRect(NewProcess.name, 0 + ww, (pictureBox1.Height - 30), Brushes.DarkKhaki); //show names of processes
+                ww += 80; //in order to draw them with spaces, its like margin-right
+            }
+            
+            while (true)
+            {
+                //Close loop on default, if the value is not changed due to available processes executions
+                bool executionFinished = true;
+                
+                //Loop through all processes until the loop ends
+                foreach (var NewProcess in multiNewProcesses)
+                {
+                    if (NewProcess.remainingTime == 0)
+                    {
+                        NewProcess.status = "Завершен";
+                        updateDataGridView(dataGridView, multiNewProcesses);
+                    }
+                    //Check if the process has any burst time left
+                    else if (NewProcess.remainingTime > 0)
+                    {
+                        //Continue the loop, as a process is executing now and we need to recheck for others
+                        executionFinished = false;
+
+                        //Check if the process remaining time is greater than quantum
+                        if (NewProcess.remainingTime > quantum)
+                        {
+                            //Process Status to Running as its Under Execution
+                            NewProcess.status = "Выполняется";
+                            updateDataGridView(dataGridView, multiNewProcesses);
+                            executionTimer(quantum);
+
+                            //Remove the quantum time from the remaining time
+                            NewProcess.remainingTime = NewProcess.remainingTime - quantum;
+
+                            //Swap Process to Ready State after execution and continue for next
+                            NewProcess.status = "Готов";
+                            updateDataGridView(dataGridView, multiNewProcesses);
+
+                            string str = (NewProcess.remainingTime).ToString();
+                            drawRect(str, 0 + hv, (pictureBox1.Height - 70) - wh, Brushes.BlueViolet); //draw rects with remaining times
+                                                     
+                        }
+                        //Only runs when the process is on its last cpu burst cycle
+                        else
+                        {
+                            //Check if the process has an IO left before it finishes its last cpu burst cycle
+                            //while (NewProcess.switchTime > 0)
+                            //{
+                            //    ioExecution(multiNewProcesses, NewProcess.ID, NewProcess.switchTime);
+                            //    NewProcess.switchTime = NewProcess.switchTime - 1;
+                            //}
+
+                            //Process Status to Running as its Under Execution
+                            NewProcess.status = "Выполняется";
+                            updateDataGridView(dataGridView, multiNewProcesses);
+                            executionTimer(NewProcess.remainingTime);
+
+                            //Set remaining time to 0, as the last cpu burst ended
+                            NewProcess.remainingTime = 0;
+
+                            //Change Process Status to Completed
+                            NewProcess.status = "Завершен";
+                            updateDataGridView(dataGridView, multiNewProcesses);
+
+                            string str = (NewProcess.remainingTime).ToString();
+                            drawRect(str, 0 + hv, (pictureBox1.Height - 70) - wh, Brushes.BlueViolet);
+                        }  
+                    }
+                    ////Execute Single IO after every CPU burst cycle
+                    //if (NewProcess.IO > 0)
+                    //{
+                    //    ioExecution(multiNewProcesses, NewProcess.ID, NewProcess.IO);
+                    //    NewProcess.IO = NewProcess.IO - 1;
+                    //}
+
+                    NewProcess last_one = multiNewProcesses.Last(); // this is for building rects in columns, so if its the last process  so the next one is gonna be set above the first one
+
+                    if (NewProcess.ID == last_one.ID)
+                    {
+                        hv = 0;
+                        wh += 30; // y 
+                    }
+                    else
+                    {
+                        hv += 80; // x                        
+                    }
+  
+                }
+
+                //When All Processes have completed their execution
+                if (executionFinished == true)
+                {                    
+                    break;
+                }
+            }
+
+
+        }        
+
+        private void drawRect(string str, int x, int y, Brush color) // drawing rects with text
+        {
+            Graphics G = pictureBox1.CreateGraphics();
+            G.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            Rectangle Rect = new Rectangle(x, y, 70, 29);
+            //G.FillRectangle(Brushes.DarkGreen, Rect);
+            G.FillRectangle(color, Rect);
+            StringFormat sf = new StringFormat();
+            sf.Alignment = StringAlignment.Center;
+            sf.LineAlignment = StringAlignment.Center;
+            G.DrawString(str, new Font("Times", 12), Brushes.White, Rect, sf);
+        }
+
+        //----------------Update Data Grid View Method-------------------------------
+        public void updateDataGridView(DataGridView dataGridView, NewProcess[] multiNewProcesses)
+        {
+            //Remove Current Data Grid Rows and Refresh it
+            dataGridView.Rows.Clear();
+            dataGridView.Refresh();
+
+            //Add Processes rows again to data grid view with updated values
+            foreach (var NewProcess in multiNewProcesses)
+            {
+                string[] row = { NewProcess.ID.ToString(), NewProcess.name, NewProcess.remainingTime.ToString(), NewProcess.switchTime.ToString(), NewProcess.status };
+                dataGridView.Rows.Add(row);
+            }
+        }
+
+        ////----------------Process IO Execution Method
+        //public void ioExecution(NewProcess[] multiNewProcesses, int id, int interupt)
+        //{
+        //    //Change Process State to Waiting when it goes to IO
+        //    foreach (var NewProcess in multiNewProcesses)
+        //    {
+        //        if (NewProcess.ID == id && NewProcess.status != "Completed")
+        //        {
+        //            NewProcess.status = "Waiting";
+        //        }
+        //    }
+        //    updateDataGridView(dataGridView, multiNewProcesses);
+
+        //    //Execute the IO for 1 second
+        //    executionTimer(1);
+
+        //    //Change Process back to Ready State after IO has completed
+        //    foreach (var NewProcess in multiNewProcesses)
+        //    {
+        //        if (NewProcess.ID == id && NewProcess.status!="Completed")
+        //        {
+        //            NewProcess.status = "Ready";
+        //        }
+        //    }
+        //    updateDataGridView(dataGridView, multiNewProcesses);
+        //}
+
+        //----------------Process Execution Timer Method
+        public void executionTimer(int tempTime)
+        {
+            int executionTime = tempTime * 1000;
+            System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+            if (executionTime == 0 || executionTime < 0)
+            {
+                return;
+            }
+            timer1.Interval = executionTime; //set timer interval to execution time. задает интервал, через который будет периодически вызываться обработчик таймера.
+            timer1.Enabled = true;
+            timer1.Start();
+            timer1.Tick += (s, e) => //adds the event/обработчик таймера/ s-sender
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+            };
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+    }
+}
