a9cae01d43a1f9089f1e5266c536cf14d420f932
dicharmed <dicharmed@mail.ru>
2019-10-10 23:26:37 +0400

    new version

---
+++ OS Project/Connection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace OS_Project
+{
+    class Connection
+    {
+        string connectionString = "Server=127.0.0.1;Port=5432;User Id=postgres;Password=1234;Database=dbUniversity;";
+        public void getData(NpgsqlCommand query)
+        {
+            NpgsqlConnection npgsqlConnection = new NpgsqlConnection(connectionString);
+            npgsqlConnection.Open();
+
+            NpgsqlDataReader reader = query.ExecuteReader();
+
+            object[] Obj = new object[5];
+
+            if (reader.HasRows)
+            {
+
+                for (int i = 0; i <= Obj.Length;)
+                {
+                    foreach (IDataRecord data in reader)
+                    {
+                        Obj[i] = data["fio"];
+                        i++;
+                    }
+                }
+
+
+            }
+
+
+            npgsqlConnection.Close();
+
+        }
+    }
+}
--- OS Project/Program.cs
+++ OS Project/Program.cs
@@ -13,7 +13,7 @@ namespace rr_program
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form3());
         }
     }
 }
