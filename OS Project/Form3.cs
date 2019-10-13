2af6461d2f5f6b22afd9ca26fe82ff9454ccdcdf
dicharmed <dicharmed@mail.ru>
2019-10-13 20:46:40 +0400

    authForm is designed

--- OS Project/Form3.cs
+++ OS Project/Form3.cs
@@ -13,16 +13,17 @@ namespace rr_program
 {
     public partial class Form3 : Form
     {
+        public string User = "student";
         public Form3()
         {
             InitializeComponent();
         }
        
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) //войти
         {
             label4.Text = "";
 
-            if (comboBox1.Text == "Студент")
+            if (User == "student")
             {
                 Student p = new Student();
                 List<Student> student = p.getStudentByLogin(getHash(textBox1.Text), getHash(textBox2.Text));
@@ -58,9 +59,7 @@ namespace rr_program
                     label4.Text = "Неверный логин/пароль!";
                     ClearTextBoxes();
                 }
-            }
-
-            
+            }            
 
         }       
    
@@ -75,16 +74,73 @@ namespace rr_program
                 result += b.ToString("x2");//форматирует строку как два строчных шестнадцатеричных символа в нижнем регистре
             }
             return result;
-        }
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            ClearTextBoxes();
-        }
+        }     
 
         private void ClearTextBoxes()
         {
             textBox1.Clear();
             textBox2.Clear();
+        }
+
+        //design
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            //form
+            this.BackColor = Color.FromArgb(0, 38, 77);
+
+            //textBoxes
+            this.textBox1.BackColor = Color.FromArgb(0, 38, 77);
+            this.textBox2.BackColor = Color.FromArgb(0, 38, 77);
+
+            //button Вход
+            this.button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 79, 153);
+            this.button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 66, 128);
+
+            //button Student
+            this.button2.BackColor = Color.FromArgb(153, 206, 255);
+            this.button2.FlatAppearance.BorderColor = Color.FromArgb(153, 206, 255);
+            this.button2.ForeColor = Color.FromArgb(0, 38, 77);
+        }
+
+        private void button3_Click(object sender, EventArgs e)//teacher
+        {
+            User = "teacher";
+
+            //teacher active
+            this.button3.BackColor = Color.FromArgb(153, 206, 255);
+            this.button3.FlatAppearance.BorderColor = Color.FromArgb(153, 206, 255);
+            this.button3.ForeColor = Color.FromArgb(0, 38, 77);
+
+            //student disactivated
+            this.button2.BackColor = Color.FromArgb(0, 38, 77);
+            this.button2.FlatAppearance.BorderColor = Color.FromArgb(255, 255, 255);
+            this.button2.ForeColor = Color.FromArgb(255, 255, 255);
+
+            ClearTextBoxes();
+            label4.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e) //student
+        {
+            User = "student";
+
+            //student active
+            this.button2.BackColor = Color.FromArgb(153, 206, 255);
+            this.button2.FlatAppearance.BorderColor = Color.FromArgb(153, 206, 255);
+            this.button2.ForeColor = Color.FromArgb(0, 38, 77);
+
+            //teacher disactivated
+            this.button3.BackColor = Color.FromArgb(0, 38, 77);
+            this.button3.FlatAppearance.BorderColor = Color.FromArgb(255, 255, 255);
+            this.button3.ForeColor = Color.FromArgb(255, 255, 255);
+
+            ClearTextBoxes();
+            label4.Text = "";
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
